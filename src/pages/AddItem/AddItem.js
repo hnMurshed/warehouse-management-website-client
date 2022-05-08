5df5e3f0ae7a362ca06bbf085152f134c147315c
Murshedul <murshedbinamir@gmail.com>
2022-05-08 15:57:25 +0600

    add item page create form for adding item

--- src/pages/AddItem/AddItem.js
+++ src/pages/AddItem/AddItem.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import './AddItem.css';
 
 const AddItem = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div className='container my-5'>
-            <h2 className='text-center'>Add New Item</h2>
+            <div className='form-container mx-auto'>
+                <h2 className='text-center mb-4'>Add New Item</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input className='w-100 mt-2' type='text' placeholder='Vegetable name*' {...register("name")} required />
+                    <textarea className='w-100 mt-3' type='text' placeholder='Item description*' {...register("description")} required />
+                    <input className='w-100 mt-2' type='number' placeholder='Item quantity*' {...register("quantity")} required />
+                    <input className='w-100 mt-3' type='number' placeholder='Item price*' {...register("price")} required />
+                    <input className='w-100 mt-3' type='text' placeholder='Supplier name*' {...register("supplier")} required />
+                    <input className='w-100 mt-3' type='text' placeholder='Item photo URL*' {...register("img")} required />
+                    <input className='w-100 mt-3' type="submit" value='Add Item' />
+                </form>
+            </div>
         </div>
     );
 };
